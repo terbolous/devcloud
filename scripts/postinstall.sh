307a85a3580c88311c5c21bf19f448559aa763b9
Chris Snow <Chris.Snow@monitise.com>
2014-02-04 22:19:39 +0000

    add postinstall function to setup xen and xapi

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -25,7 +25,7 @@ install_packages() {
   apt-get --no-install-recommends -q -y --force-yes install python bzip2 sed gawk diffutils grep gzip less tar telnet wget zip unzip sudo
 
   # dev tools, ssh, nfs
-  apt-get --no-install-recommends -q -y --force-yes install git vim tcpdump ebtables iptables openssl openssh-server openjdk-6-jdk genisoimage python-pip nfs-kernel-server
+  apt-get --no-install-recommends -q -y --force-yes install git vim tcpdump ebtables iptables openssl openssh-server openjdk-7-jdk genisoimage python-pip nfs-kernel-server
 
   # mysql with root password=password
   echo 'mysql-server-<version> mysql-server/root_password password password' | debconf-set-selections 
@@ -37,7 +37,44 @@ install_packages() {
   apt-get --no-install-recommends -q -y --force-yes install xcp-networkd
   apt-get --no-install-recommends -q -y --force-yes install linux-headers-3.2.0-4-686-pae xen-hypervisor-4.1-i386 xcp-xapi xcp-xe xcp-guest-templates xcp-vncterm xen-tools blktap-utils blktap-dkms qemu-keymaps qemu-utils
 
+  pip install mysql-connector-python
 }
+
+
+fix_mysql_password() {
+  mysql -u root --password=password \
+        -e "SET PASSWORD FOR root@localhost=PASSWORD('');"
+}
+
+setup_xen_and_xapi() {
+  echo "bridge" > /etc/xcp/network.conf
+  update-rc.d xendomains disable
+  echo TOOLSTACK=xapi > /etc/default/xen
+  sed -i 's/GRUB_DEFAULT=.\+/GRUB_DEFAULT="Xen 4.1-i386"/' /etc/default/grub
+  sed -i 's/GRUB_CMDLINE_LINUX=.\+/GRUB_CMDLINE_LINUX="apparmor=0"\nGRUB_CMDLINE_XEN="dom0_mem=400M,max:500M dom0_max_vcpus=1"/' /etc/default/grub
+  update-grub
+  sed -i 's/VNCTERM_LISTEN=.\+/VNCTERM_LISTEN="-v 0.0.0.0:1"/' /usr/lib/xcp/lib/vncterm-wrapper
+  cat > /usr/lib/xcp/plugins/echo << EOF
+!/usr/bin/env python
+
+# Simple XenAPI plugin
+import XenAPIPlugin, time
+
+def main(session, args):
+    if args.has_key("sleep"):
+        secs = int(args["sleep"])
+        time.sleep(secs)
+    return "args were: %s" % (repr(args))
+
+if __name__ == "__main__":
+    XenAPIPlugin.dispatch({"main": main})
+EOF
+
+  chmod -R 777 /usr/lib/xcp
+  mkdir -p /root/.ssh
+  ssh-keygen -A -q
+}
+
 
 fix_locale() {
   cat << EOF >> /etc/default/locale
@@ -61,6 +98,8 @@ fix_grub() {
 begin=$(date +%s)
 
 install_packages
+fix_mysql_password
+setup_xen_and_xapi
 fix_locale
 fix_grub
 
